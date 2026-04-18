e8cc1c3572bc3a2dae97461086885247806f3e32
Σrebe - Romain GERARD <erebe@erebe.eu>
2026-04-18 23:24:28 +0200

    bump

--- scripts/generic_vids.sh
+++ scripts/generic_vids.sh
@@ -8,8 +8,7 @@ mkdir "${video_name}"
 video_path=${video_name}/${video_name}
 
 yt-dlp -o ${video_path}.ori.mp4 ${video_url}
-video_full=$(find ${video_name}/ -type f -name "${video_name}")
-ffmpeg -i ${video_full} -c:v libx265 -crf 24 -preset slow -tag:v hvc1 -c:a aac -b:a 192k ${video_path}.mp4
+ffmpeg -i ${video_path}.ori.mp4 -c:v libx265 -crf 24 -preset slow -tag:v hvc1 -c:a aac -b:a 192k ${video_path}.mp4
 ffmpegthumbnailer -i "${video_path}.mp4" -o "${video_path}.thumbnail.png" -s 500
 
 cat <<EOF > ${video_path}.js
