cb892b6db6b83e71f840d9842b8c2a2129964a3e
Σrebe - Romain GERARD <erebe@erebe.eu>
2026-04-18 23:13:26 +0200

    bump

--- scripts/generic_vids.sh
+++ scripts/generic_vids.sh
@@ -7,15 +7,15 @@ mkdir "${video_name}"
 
 video_path=${video_name}/${video_name}
 
-yt-dlp -o ${video_path} ${video_url}
+yt-dlp -o ${video_path}.ori.mp4 ${video_url}
 video_full=$(find ${video_name}/ -type f -name "${video_name}")
-ffmpeg -i ${video_full} -c:v libx265 -crf 24 -preset slow -tag:v hvc1 -c:a aac -b:a 192k ${video_full}.mp4
-ffmpegthumbnailer -i "${video_full}" -o "${video_path}.thumbnail.png" -s 500
+ffmpeg -i ${video_full} -c:v libx265 -crf 24 -preset slow -tag:v hvc1 -c:a aac -b:a 192k ${video_path}.mp4
+ffmpegthumbnailer -i "${video_path}.mp4" -o "${video_path}.thumbnail.png" -s 500
 
 cat <<EOF > ${video_path}.js
 {
   "name": "${video_name}",
-  "url": "/videos/${video_full}.mp4",
+  "url": "/videos/${video_path}.mp4",
   "tags": [],
   "original": "${video_url}",
   "thumbnail": "/videos/${video_path}.thumbnail.png",
